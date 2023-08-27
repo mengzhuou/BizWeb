fea7df75ca6c9a290c6926fab4d75ca01dcc4f7d
mengzhuou <mou30@gatech.edu>
2023-08-27 16:34:58 -0400

    added middleware

--- routes/root.js
+++ routes/root.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
-//^ means at the beginning of string only, $ means the end, this means we only look for /, which is our root
 router.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
 })
