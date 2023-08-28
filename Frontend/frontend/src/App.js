1b46ae2da7aaa4cdc5d52800be77ff3ad26c9d40
mengzhuou <mou30@gatech.edu>
2023-08-27 21:13:20 -0400

    created frontend

---
+++ Frontend/frontend/src/App.js
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Public from './components/Public'
+import Login from './components/Login';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ Frontend/frontend/src/components/Public.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+
+const Public = () => {
+    const content = (
+        <section className="public">
+            <header>
+                <span className="nowrap">Log In</span>
+            </header>
+            <body>
+                
+            </body>
+                {/* <Link to="/login">Employee Login</Link> */}
+        </section>
+    )
+    return content
+}
+export default Public
