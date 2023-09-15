3bd52ff3bc2b415f5c147fb72daf9bf791a01d02
mengzhuou <mou30@gatech.edu>
2023-09-15 18:26:23 -0400

    added frontend

--- Frontend/frontend/src/index.js
+++ Frontend/frontend/src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
