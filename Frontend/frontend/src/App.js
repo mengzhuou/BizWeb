53900db1039d795dd77ed8f56c7ec1916ffb8579
mengzhuou <mou30@gatech.edu>
2023-09-19 22:04:12 -0400

    added buttons in menu

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -3,6 +3,9 @@ import Layout from './components/Layout'
 import Public from './components/Public'
 import Login from './components/Login';
 import Menu from './components/Menu';
+import RegisterClient from './components/RegisterClient';
+import ExistingClient from './components/ExistingClient';
+import DirectProject from './components/DirectProject';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
         <Route path="menu" element={<Menu />} />
+        <Route path="registerClient" element={<RegisterClient />} />
+        <Route path="existingClient" element={<ExistingClient />} />
+        <Route path="directProject" element={<DirectProject />} />
       </Route>
     </Routes>
   );
