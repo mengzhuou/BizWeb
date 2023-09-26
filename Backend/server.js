1c7bcc5e427052223c81d97f1f5184726bb49940
KaiHackney <kaiahackney@gmail.com>
2023-09-26 17:39:19 -0400

    finished auth for user
    still need to create a login page, but on the backend a user can be logged in or out

--- Backend/server.js
+++ Backend/server.js
@@ -12,46 +12,42 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3500;
 
-console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV)
 
-// Connect to the database
-connectDB();
+connectDB()
 
-// Initialize Express app
-const app = express();
+app.use(logger)
 
-// Middleware
-app.use(logger);
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(cookieParser());
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use(cors(corsOptions))
 
-// Routes
-app.use("/", require("./routes/root"));
-app.use("/users", require("./routes/userRoutes"));
-app.use("/clients", require("./routes/clientRoutes"));
+app.use(express.json())
 
-// 404 Not Found
-app.all("*", (req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({ message: "404 Not Found" });
-  } else {
-    res.type("txt").send("404 Not Found");
-  }
-});
+app.use(cookieParser())
 
-// Error handling
-app.use(errorHandler);
+app.use('/', express.static(path.join(__dirname, 'public')))
 
-// Start the server
-mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-});
+app.use('/', require('./routes/root'))
+app.use('/users', require('./routes/userRoutes'))
+// app.use('/notes', require('./routes/noteRoutes'))
+app.use('/auth', require('./routes/authRoutes'))
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+app.use(errorHandler)
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
 
 mongoose.connection.on("error", (err) => {
   console.log(err);
