37f6333285f3213f6a1a2e2cfd7574e57b276b79
KaiHackney <kaiahackney@gmail.com>
2023-10-01 23:41:07 -0400

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -51,13 +51,13 @@ app.use(errorHandler);
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  });
-  
-  mongoose.connection.on("error", (err) => {
-    console.log(err);
-    logEvents(
-    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-    "mongoErrLog.log"
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+  `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+  "mongoErrLog.log"
   );
 });
   
