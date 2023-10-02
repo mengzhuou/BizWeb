69d1151a06044d8d4d4804e9dc7c9b9b59fa7667
KaiHackney <kaiahackney@gmail.com>
2023-10-01 23:41:28 -0400

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -56,8 +56,8 @@ mongoose.connection.once("open", () => {
 mongoose.connection.on("error", (err) => {
   console.log(err);
   logEvents(
-  `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-  "mongoErrLog.log"
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
   );
 });
   
