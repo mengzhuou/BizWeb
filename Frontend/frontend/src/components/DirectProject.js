2d3b8ab201f89dfc5f8feed6369be015379b2009
KaiHackney <113050521+KaiHackney@users.noreply.github.com>
2023-10-02 09:40:10 -0400

    Direct project page#9 (#9)
    * Update mongoose
    
    * adding basic login functions
    
    * finished auth for user
    
    still need to create a login page, but on the backend a user can be logged in or out
    
    * dependencies
    
    * finished auth for user
    
    still need to create a login page, but on the backend a user can be logged in or out
    
    * directProject Page
    
    * finished auth for user
    
    still need to create a login page, but on the backend a user can be logged in or out
    
    * finished auth for user
    
    still need to create a login page, but on the backend a user can be logged in or out
    
    * style updates
    
    * Update mongoose
    
    * adding basic login functions
    
    * finished auth for user
    
    still need to create a login page, but on the backend a user can be logged in or out
    
    * dependencies
    
    * finished auth for user
    
    still need to create a login page, but on the backend a user can be logged in or out
    
    * directProject Page
    
    * finished auth for user
    
    still need to create a login page, but on the backend a user can be logged in or out
    
    * finished auth for user
    
    still need to create a login page, but on the backend a user can be logged in or out
    
    * style updates
    
    * finished auth for user
    
    still need to create a login page, but on the backend a user can be logged in or out
    
    * finished auth for user
    
    still need to create a login page, but on the backend a user can be logged in or out
    
    * finished auth for user
    
    still need to create a login page, but on the backend a user can be logged in or out
    
    * rebase
    
    * quick fix
    
    * Update server.js
    
    * Update server.js
    
    * Update server.js
    
    * Update server.js
    
    * Update DirectProject.js
    
    ---------
    
    Co-authored-by: Rishabh Kanodia <55036111+RishabhK9@users.noreply.github.com>

--- Frontend/frontend/src/components/DirectProject.js
+++ Frontend/frontend/src/components/DirectProject.js
@@ -1,7 +1,54 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import "./Menu.css";
+
+
 
 const DirectProject = () => {
-  const content = <section className="public"></section>;
+
+  const getInitialState = () => {
+    const value = "Project1";
+    return value;
+  };
+
+  const [value, setValue] = useState(getInitialState);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+
+  const navigate = useNavigate();
+  const handleProject = () => {
+    navigate("/directProject/" + value);
+  };
+  const content = (
+    <section className="public">
+      <header>
+        <Link to="/" className="topNavBar">
+          Log Out
+        </Link>      
+        <Link to="/menu" className="topNavBar">
+          Menu
+        </Link>      
+      </header>
+      <main className="public__main">
+        <h1>Direct Project</h1>
+        <div className="menuButtonContainer">
+          <h1>Select a Project</h1>
+          <select className="menuButton" value={value} onChange={handleChange}>
+            <option value="Project1">Project 1</option>
+            <option value="Project2">Project 2</option>
+            <option value="Project3">Project 3</option>
+          </select>
+          <button className="menuButton" onClick={handleProject}>
+            Submit
+          </button>
+        </div>
+      </main>
+    </section>
+  );
   return content;
 };
 export default DirectProject;
+
