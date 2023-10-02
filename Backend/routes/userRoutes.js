f0a56334f8b079d7d47b1eab728f91e611082f22
KaiHackney <kaiahackney@gmail.com>
2023-10-01 23:38:32 -0400

    rebase

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const usersController = require('../controllers/usersController')
-const verifyJWT = require('../middleware/verifyJWT')
-
-router.use(verifyJWT)
 
 router.route('/')
     .get(usersController.getAllUsers)
--- Backend/server.js
+++ Backend/server.js
@@ -12,47 +12,51 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3500;
 
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 
-connectDB()
+// Connect to the database
+connectDB();
 
-app.use(logger)
+// Initialize Express app
+const app = express();
 
-app.use(cors(corsOptions))
+// Middleware
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", express.static(path.join(__dirname, "public")));
 
-app.use(express.json())
+// Routes
+app.use("/", require("./routes/root"));
+app.use("/users", require("./routes/userRoutes"));
+app.use("/clients", require("./routes/clientRoutes"));
 
-app.use(cookieParser())
-
-app.use('/', express.static(path.join(__dirname, 'public')))
-
-app.use('/', require('./routes/root'))
-app.use('/users', require('./routes/userRoutes'))
-// app.use('/notes', require('./routes/noteRoutes'))
-app.use('/auth', require('./routes/authRoutes'))
-
-app.all('*', (req, res) => {
-    res.status(404)
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if (req.accepts('json')) {
-        res.json({ message: '404 Not Found' })
-    } else {
-        res.type('txt').send('404 Not Found')
-    }
-})
-
-app.use(errorHandler)
-
-mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB')
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-})
-
-mongoose.connection.on("error", (err) => {
-  console.log(err);
-  logEvents(
-    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-    "mongoErrLog.log"
-  );
+// 404 Not Found
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
+
+// Error handling
+app.use(errorHandler);
+
+// Start the server
+mongoose.connection.once("open", () => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  });
+  
+  mongoose.connection.on("error", (err) => {
+    console.log(err);
+    logEvents(
+      `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+      "mongoErrLog.log"
+    );
+  });  
