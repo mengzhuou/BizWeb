2b2b41dcc10d3ba7958b5d3ef53a70beadd03809
dylan kelly <dkelly66@gatech.edu>
2023-10-03 18:52:26 -0400

    Changed navbar to match Figma designs

--- Frontend/frontend/src/components/NavBar.js
+++ Frontend/frontend/src/components/NavBar.js
@@ -4,11 +4,6 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <div>
-      <a href="/" class="">
-        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-          BizWeb
-        </span>
-      </a>
       <Link to="/" class="float-right" className="topNavBar float-right">
         Log Out
       </Link>
