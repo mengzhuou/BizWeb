9defb177a08dcb4c604fd5d79851888b8402e02f
dylan kelly <dkelly66@gatech.edu>
2023-10-03 00:55:05 -0400

    Installed Tailwind, shifted elements to right

--- Frontend/frontend/src/components/NavBar.js
+++ Frontend/frontend/src/components/NavBar.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div class="border-solid border-sky-500">
-      <Link to="/menu" className="topNavBar">
-        Menu
-      </Link>
-      <Link to="/" className="topNavBar">
-        Management
-      </Link>
+    <div class="border-solid border-color-500">
       <Link to="/" class="float-right" className="topNavBar float-right">
         Log Out
+      </Link>
+      <Link to="/" className="topNavBar float-right">
+        Menu
+      </Link>
+      <Link to="/" className="topNavBar float-right">
+        Management
       </Link>
     </div>
   );
