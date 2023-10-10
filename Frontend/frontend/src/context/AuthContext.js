eb44e13ec0dad52eeefae14b48bea2815b13af5a
Rishabh Kanodia <55036111+RishabhK9@users.noreply.github.com>
2023-10-10 15:05:51 -0400

    Working save state

--- Frontend/frontend/src/context/AuthContext.js
+++ Frontend/frontend/src/context/AuthContext.js
@@ -2,7 +2,7 @@ import { createContext, useReducer } from 'react'
 
 export const AuthContext = createContext()
 
-export const authReducer = (state,action) => {
+export const authReducer = (state , action) => {
     switch (action.type) {
         case 'LOGIN':
             return { user: action.payload }
@@ -13,7 +13,7 @@ export const authReducer = (state,action) => {
     }
 }
 
-export const AuthContextProvider = () => ({ children }) => {
+export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, {
         user: null
     })
