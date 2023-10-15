fd7b5d7c0a5ad1290251e097249343fae867fd7c
mengzhuou <mou30@gatech.edu>
2023-10-14 23:40:19 -0400

    configured windowMs value

--- Backend/middleware/rateLimiter.js
+++ Backend/middleware/rateLimiter.js
@@ -2,8 +2,8 @@ const rateLimit = require('express-rate-limit')
 
 const limiter = rateLimit({
     max: 10,
-    //10 seconds
-    windowMS: 10000,
+    //default is 60000 means 1 minute. Current wait time is around 10 seconds.
+    windowMs: 17000,
     message: "You can't make any more request at the moment. Try again later."
 })
 
