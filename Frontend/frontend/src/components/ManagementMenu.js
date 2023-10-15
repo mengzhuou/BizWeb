858230723ac8112ee5e5cf298ec63c46daed4311
mengzhuou <mou30@gatech.edu>
2023-10-15 00:54:29 -0400

    commit all files

---
+++ Frontend/frontend/src/components/ManagementMenu.js
@@ -0,0 +1,40 @@
+import { useNavigate } from "react-router-dom";
+
+const ManagementMenu = () => {
+  const navigate = useNavigate();
+  const handleRegisterEmployee = () => {
+    navigate("/registerEmployee");
+  };
+
+  const handleExistingEmployee = () => {
+    navigate("/manageEmployee");
+  };
+
+  const handleExistingClient = () => {
+    navigate("/manageClient");
+  };
+
+  const handleFinancialReport = () => {
+    navigate("/financialReport");
+  };
+
+  const content = (
+        <div className="menuButtonContainer">
+          <button className="menuButton" onClick={handleRegisterEmployee}>
+            Register a New Employee
+          </button>
+          <button className="menuButton" onClick={handleExistingEmployee}>
+            Manage Employees
+          </button>
+          <button className="menuButton" onClick={handleExistingClient}>
+            Manage Clients
+          </button>
+          <button className="menuButton" onClick={handleFinancialReport}>
+            Financial Reports
+          </button>
+        </div>
+    
+  );
+  return content;
+};
+export default ManagementMenu;
