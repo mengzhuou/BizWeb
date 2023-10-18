f158c9a880b2c43756ac25c0e745f467cbe5cbb5
dylan kelly <dkelly66@gatech.edu>
2023-10-17 21:58:30 -0400

    Added secondary phone number checking

--- Frontend/frontend/src/components/RegisterClient.js
+++ Frontend/frontend/src/components/RegisterClient.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import GenderDropdown from "./GenderDropdown";
+import { useNavigate } from "react-router-dom";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 import Axios from "axios";
-import Navbar from "./NavBar";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function RegisterClient() {
   const [firstName, setFirstName] = useState("");
@@ -12,7 +12,8 @@ function RegisterClient() {
   const [email, setEmail] = useState("");
   const [birthday, setBirthday] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
-  const [gender, setGender] = useState("");
+  const [secondaryPhoneNumber, setSecondaryPhoneNumber] = useState("");
+  const navigate = useNavigate();
   const [emailError, setEmailError] = useState("");
   const [presenceError, setPresenceError] = useState("");
 
@@ -28,13 +29,13 @@ function RegisterClient() {
 
     if (id === "email") {
       // Email format validation
-      const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
-      if (!emailPattern.test(value)) {
-        setEmailError("Invalid email format");
-      } else {
-        setEmail(value);
-        setEmailError("");
-      }
+      // const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+      // if (!emailPattern.test(value)) {
+      //   setEmailError("Invalid email format");
+      // } else {
+      setEmail(value);
+      // setEmailError("");
+      // }
     }
     if (id === "birthday") {
       setBirthday(value);
@@ -42,16 +43,16 @@ function RegisterClient() {
     if (id === "phoneNumber") {
       setPhoneNumber(value);
     }
-    if (id === "gender") {
-      setGender(value);
+    if (id === "secondaryPhoneNumber") {
+      setSecondaryPhoneNumber(value);
     }
   };
 
   const handleSubmit = () => {
     // Create an object to hold the client data
-
-    if (emailError) {
-      setPresenceError("Email format is invalid");
+    const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    if (!emailPattern.test(email)) {
+      setPresenceError("Invalid email format");
       return;
     } else if (!firstName.trim()) {
       setPresenceError("First name cannot be blank.");
@@ -65,136 +66,150 @@ function RegisterClient() {
     } else if (!phoneNumber.trim()) {
       setPresenceError("Phone number cannot be blank.");
       return;
-    } else if (!gender.trim()) {
-      setPresenceError("Gender cannot be blank");
-      return;
     } else {
       setPresenceError("");
     }
-
+    if (!secondaryPhoneNumber.trim()) {
+      setSecondaryPhoneNumber("");
+    }
     const clientData = {
       firstName,
       lastName,
       email,
       birthday,
       phoneNumber,
-      gender,
+      secondaryPhoneNumber,
     };
 
-    Axios.post("http://localhost:3500/clients", clientData)
+    Axios.post("/clients", clientData)
       .then((response) => {
-        console.log("Client created successfully", response.data);
+        toast.success("Successfully added to the database");
+        console.log("Successfully added to the database");
+        const newClientId = response.data._id;
+        navigate(`/displayClient/${newClientId}`);
       })
       .catch((error) => {
-        if (error.response === undefined) {
-          console.error("Server is not running");
-        }
-        if (error.response.status === 400) {
-          if (error.response.data.message === "All fields are required") {
-            console.log(error.response.data.message);
+        let errorMessage = error.response.data.message;
+
+        if (error.response) {
+          switch (error.response.status) {
+            case 400:
+              errorMessage = error.response.data.message;
+              break;
+            case 409:
+              errorMessage = error.response.data.message;
+              break;
+            default:
+              errorMessage = "Unhandled error status: " + error.response.status;
           }
-        }
-        // If the server responded with an error, check the status code
-        if (error.response.status === 409) {
-          // Check the error response data for specific messages
-          if (
-            error.response.data.message === "Email has already been registered."
-          ) {
-            console.log(error.response.data.message);
-          } else if (
-            error.response.data.message ===
-            "Phone number has already been registered."
-          ) {
-            console.log(error.response.data.message);
-          } else {
-            console.error("Error creating client", error);
-          }
+          toast.error(errorMessage);
         } else {
-          console.error("Network error or other issues", error);
+          toast.error("Network error or other issues");
         }
       });
   };
 
   return (
-    <section className="public">
-      <header>
-        <Navbar />
-      </header>
-      <main className="public__main">
-        <h1>Register Client</h1>
-        <div className="text-black">
+    <>
+      <h1>Register Client</h1>
+      <div>
+        <div>
           <div>
-            <div>
-              <label htmlFor="firstName">First Name: </label>
-              <input
-                type="text"
-                value={firstName}
-                onChange={(e) => handleInputChange(e)}
-                id="firstName"
-                placeholder="First name"
-              />
-            </div>
-            <div>
-              <label htmlFor="lastName">Last Name: </label>
-              <input
-                type="text"
-                id="lastName"
-                value={lastName}
-                onChange={(e) => handleInputChange(e)}
-                placeholder="Last name"
-              />
-            </div>
-            <div>
-              <label htmlFor="email">Email: </label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => handleInputChange(e)}
-                placeholder="Email"
-              />
-            </div>
-            <div>
-              <label htmlFor="birthday">Date Of Birth: </label>
-              <input
-                type="date"
-                id="birthday"
-                value={birthday}
-                onChange={(e) => handleInputChange(e)}
-                placeholder="MM/DD/YYYY"
-              />
-            </div>
-            <div>
-              <label htmlFor="phoneNumber">Phone Number: </label>
-              <PhoneInput
-                inputProps={{
-                  name: "phoneNumber",
-                  id: "phoneNumber",
-                }}
-                country={"us"}
-                value={phoneNumber}
-                onChange={(value) => setPhoneNumber(value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="gender">Select Gender: </label>
-              <GenderDropdown setGender={setGender} gender={gender} />
-            </div>
+            <label htmlFor="firstName">First Name: </label>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => handleInputChange(e)}
+              id="firstName"
+              placeholder="First name"
+            />
           </div>
           <div>
-            <button
-              onClick={() => handleSubmit()}
-              type="submit"
-              className="btn"
-            >
-              Register
-            </button>
+            <label htmlFor="lastName">Last Name: </label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={(e) => handleInputChange(e)}
+              placeholder="Last name"
+            />
           </div>
-          <p style={{ color: "red" }}>{presenceError}</p>{" "}
-          {/* Display error message */}
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => handleInputChange(e)}
+              placeholder="Email"
+            />
+          </div>
+          <div>
+            <label htmlFor="birthday">Date Of Birth: </label>
+            <input
+              type="date"
+              id="birthday"
+              value={birthday}
+              onChange={(e) => handleInputChange(e)}
+              placeholder="MM/DD/YYYY"
+              min="1900-01-01"
+              max={new Date().toISOString().split("T")[0]}
+            />
+          </div>
+          <div>
+            <label htmlFor="phoneNumber">Primary Phone Number: </label>
+            <PhoneInput
+              inputProps={{
+                name: "phoneNumber",
+                id: "phoneNumber",
+              }}
+              country={"us"}
+              onlyCountries={["us"]}
+              value={phoneNumber}
+              onChange={(value) => setPhoneNumber(value)}
+              placeholder="9 (999) 999-9999"
+              containerStyle={{
+                height: "50px",
+              }}
+              inputStyle={{
+                height: "50px",
+                fontSize: "16px",
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor="secondaryPhoneNumber">
+              Secondary Phone Number:{" "}
+            </label>
+            <PhoneInput
+              inputProps={{
+                name: "secondaryPhoneNumber",
+                id: "secondaryPhoneNumber",
+              }}
+              country={"us"}
+              onlyCountries={["us"]}
+              value={secondaryPhoneNumber}
+              onChange={(value) => setSecondaryPhoneNumber(value)}
+              placeholder="9 (999) 999-9999"
+              containerStyle={{
+                height: "50px",
+              }}
+              inputStyle={{
+                height: "50px",
+                fontSize: "16px",
+              }}
+            />
+          </div>
         </div>
-      </main>
-    </section>
+        <div>
+          <button onClick={() => handleSubmit()} type="submit" className="btn">
+            Register
+          </button>
+          <ToastContainer />
+        </div>
+        <p style={{ color: "red" }}>{presenceError}</p>{" "}
+      </div>
+    </>
   );
 }
 
