dc4c8375a91a88fec761385864b3f677e20d7da6
dylan kelly <dkelly66@gatech.edu>
2023-10-27 17:05:38 -0400

    Center error message, changed bg color

--- Frontend/frontend/src/components/RegisterClient.js
+++ Frontend/frontend/src/components/RegisterClient.js
@@ -102,114 +102,118 @@ function RegisterClient() {
 
   return (
     <>
-        <header className="fmy-2">
-          <h1 className="text-xl font-bold">Register Client</h1>
-        </header>
-        <div className="flex flex-col items-center justify-center p-2">
-          <div>
-            <label htmlFor="firstName">First Name: </label>
-            <input
-              type="text"
-              value={firstName}
-              onChange={(e) => handleInputChange(e)}
-              id="firstName"
-              placeholder="First name"
-              className="className= mb-3 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            />
-          </div>
-          <div>
-            <label htmlFor="lastName">Last Name: </label>
-            <input
-              type="text"
-              id="lastName"
-              value={lastName}
-              onChange={(e) => handleInputChange(e)}
-              placeholder="Last name"
-              className="className= mb-3 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            />
-          </div>
-          <div className="ml-9">
-            <label htmlFor="email">Email: </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => handleInputChange(e)}
-              placeholder="Email"
-              className="className= mb-3 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            />
-          </div>
-          <div className="mr-20">
-            <label htmlFor="birthday">Date Of Birth: </label>
-            <input
-              type="date"
-              id="birthday"
-              value={birthday}
-              onChange={(e) => handleInputChange(e)}
-              placeholder="MM/DD/YYYY"
-              min="1900-01-01"
-              max={new Date().toISOString().split("T")[0]}
-              className="className= mb-3 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            />
-          </div>
-          <div className="mr-7">
-            <label htmlFor="phoneNumber">Primary Phone Number: </label>
-            <PhoneInput
-              inputProps={{
-                name: "phoneNumber",
-                id: "phoneNumber",
-              }}
-              country={"us"}
-              onlyCountries={["us"]}
-              value={phoneNumber}
-              onChange={(value) => setPhoneNumber(value)}
-              placeholder="9 (999) 999-9999"
-              containerStyle={{
-                height: "30px",
-                width:"fit-content",
-                display: "inline-block"
-              }}
-              inputStyle={{
-                height: "30px",
-                width:"fit-content",
-                fontSize: "16px",
-              }}
-            />
-          </div>
-          <div className="mt-3 mr-12">
-            <label htmlFor="secondaryPhoneNumber">
-              Secondary Phone Number:{" "}
-            </label>
-            <PhoneInput
-              inputProps={{
-                name: "secondaryPhoneNumber",
-                id: "secondaryPhoneNumber",
-              }}
-              country={"us"}
-              onlyCountries={["us"]}
-              value={secondaryPhoneNumber}
-              onChange={(value) => setSecondaryPhoneNumber(value)}
-              placeholder="9 (999) 999-9999"
-              containerStyle={{
-                height: "30px",
-                width:"fit-content",
-                display: "inline-block"
-              }}
-              inputStyle={{
-                width:"fit-content",
-                height: "30px",
-                fontSize: "16px",
-              }}
-            />
-          </div>
+      <header className="fmy-2">
+        <h1 className="text-xl font-bold">Register Client</h1>
+      </header>
+      <div className="flex flex-col items-center justify-center p-2">
+        <div>
+          <label htmlFor="firstName">First Name: </label>
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => handleInputChange(e)}
+            id="firstName"
+            placeholder="First name"
+            className="className= mb-3 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
         </div>
-        <div className="flex justify-center">
-          <button onClick={() => handleSubmit()} type="submit" className="mt-3 ml-72 btn">
-            Register
-          </button>
-          <ToastContainer />
+        <div>
+          <label htmlFor="lastName">Last Name: </label>
+          <input
+            type="text"
+            id="lastName"
+            value={lastName}
+            onChange={(e) => handleInputChange(e)}
+            placeholder="Last name"
+            className="className= mb-3 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
         </div>
-        <p style={{ color: "red" }}>{presenceError}</p>{" "}
+        <div className="ml-9">
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => handleInputChange(e)}
+            placeholder="Email"
+            className="className= mb-3 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
+        </div>
+        <div className="mr-20">
+          <label htmlFor="birthday">Date Of Birth: </label>
+          <input
+            type="date"
+            id="birthday"
+            value={birthday}
+            onChange={(e) => handleInputChange(e)}
+            placeholder="MM/DD/YYYY"
+            min="1900-01-01"
+            max={new Date().toISOString().split("T")[0]}
+            className="className= mb-3 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
+        </div>
+        <div className="mr-7">
+          <label htmlFor="phoneNumber">Primary Phone Number: </label>
+          <PhoneInput
+            inputProps={{
+              name: "phoneNumber",
+              id: "phoneNumber",
+            }}
+            country={"us"}
+            onlyCountries={["us"]}
+            value={phoneNumber}
+            onChange={(value) => setPhoneNumber(value)}
+            placeholder="9 (999) 999-9999"
+            containerStyle={{
+              height: "30px",
+              width: "fit-content",
+              display: "inline-block",
+            }}
+            inputStyle={{
+              height: "30px",
+              width: "fit-content",
+              fontSize: "16px",
+            }}
+          />
+        </div>
+        <div className="mt-3 mr-12">
+          <label htmlFor="secondaryPhoneNumber">Secondary Phone Number: </label>
+          <PhoneInput
+            inputProps={{
+              name: "secondaryPhoneNumber",
+              id: "secondaryPhoneNumber",
+            }}
+            country={"us"}
+            onlyCountries={["us"]}
+            value={secondaryPhoneNumber}
+            onChange={(value) => setSecondaryPhoneNumber(value)}
+            placeholder="9 (999) 999-9999"
+            containerStyle={{
+              height: "30px",
+              width: "fit-content",
+              display: "inline-block",
+            }}
+            inputStyle={{
+              width: "fit-content",
+              height: "30px",
+              fontSize: "16px",
+            }}
+          />
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <button
+          onClick={() => handleSubmit()}
+          type="submit"
+          className="mt-3 ml-72 btn"
+        >
+          Register
+        </button>
+        <ToastContainer />
+      </div>
+      <p className="mt-5 flex justify-center" style={{ color: "red" }}>
+        {presenceError}
+      </p>{" "}
     </>
   );
 }
