6bfcf604b4c7052ccf01600f08aa0b40e23e26ee
Duc Nguyen <dnguyen416@gatech.edu>
2023-10-30 20:40:21 -0400

    Fix seach role in roles & refractor verifyJWT.js

--- Backend/middleware/verifyJWT.js
+++ Backend/middleware/verifyJWT.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken')
 
-const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers.authorization || req.headers.Authorization
+const verifyJWT = (requiredRole) => {
+    return (req, res, next) => {
+      const authHeader = req.headers.authorization || req.headers.Authorization
 
     if (!authHeader?.startsWith('Bearer ')) {
         return res.status(401).json({ message: 'Unauthorized no token' })
@@ -14,36 +15,18 @@ const verifyJWT = (req, res, next) => {
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
             if (err) return res.status(403).json({ message: 'Forbidden' })
-            req.user = decoded.UserInfo.username
-            req.roles = decoded.UserInfo.roles
-            next()
+            else if (decoded.UserInfo.roles.includes(requiredRole)) {
+              req.user = decoded.UserInfo.username
+              req.roles = decoded.UserInfo.roles
+              next()
+            } else {
+              return res.status(403).send('You do not have the required role');
+            }
         }        
     )
+    }
 }
 
-const requireRole = (role) => {
-    return (req, res, next) => {
-      const token = req.headers['authorization']?.split(' ')[1];
-  
-      if (token) {
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-          if (err) {
-            return res.status(401).send('Invalid token');
-          } else if (decoded.UserInfo.roles === role) {
-            
-            req.user = decoded; // add the decoded user info to the request object
-            next(); // proceed to the next middleware or route handler
-          } else {
-            return res.status(403).send('You do not have the required role');
-          }
-        });
-      } else {
-        return res.status(403).send('No token provided');
-      }
-    };
-  };
-
 module.exports = {
-    verifyJWT,
-    requireRole
+    verifyJWT
 } 
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,15 +2,14 @@ const express = require('express')
 const router = express.Router()
 const usersController = require('../controllers/usersController')
 const {verifyJWT} = require('../middleware/verifyJWT')
-const {requireRole} = require('../middleware/verifyJWT')
 
-router.use(verifyJWT)
+router.use(verifyJWT('manager'))
 
 router.route('/')
-    .get(requireRole('manager'), usersController.getAllUsers)
-    .post(requireRole('manager'), usersController.createNewUser)
-    .patch(requireRole('manager'), usersController.updateUser)
-    .delete(requireRole('manager'), usersController.deleteUser)
+    .get(usersController.getAllUsers)
+    .post(usersController.createNewUser)
+    .patch(usersController.updateUser)
+    .delete(usersController.deleteUser)
 
 router.route('/:id').get(usersController.getUser)
 
