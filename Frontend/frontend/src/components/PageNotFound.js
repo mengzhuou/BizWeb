8f2f591c07884fddfca4a1969dda1cf8efce6c42
KaiHackney <kaiahackney@gmail.com>
2023-11-01 16:50:34 -0400

    Update PageNotFound.js

--- Frontend/frontend/src/components/PageNotFound.js
+++ Frontend/frontend/src/components/PageNotFound.js
@@ -4,7 +4,7 @@ import "./Menu.css";
 const PageNotFound = () => {
   const content = (
     <>
-      <body className="bg-grey">
+      <body>
       <img src="404.png" alt="404 Error" width="100%" height="100%" className="border-4 border-black"/>
       </body>
     </>
